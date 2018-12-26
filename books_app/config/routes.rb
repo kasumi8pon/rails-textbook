942fdd91a2d1e3385c132640d55b935243d82079
kasumi8pon <all.splashing@gmail.com>
2018-12-26 21:50:01 +0900

    add omniauth-facebook

--- books_app/config/routes.rb
+++ books_app/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: "books#index"
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_121706) do
+ActiveRecord::Schema.define(version: 2018_12_26_022609) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2018_11_28_121706) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
